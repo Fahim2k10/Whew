4c6b2bfb04fbb0cbeb351c631e30ed051f5d9cca
Fahim <chowdhuryfahimali@gmail.com>
2025-09-03 22:27:54 +0530

    open graph

--- app/layout.js
+++ app/layout.js
@@ -5,6 +5,28 @@ import "./globals.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
+export const metadata = {
+  title: "Whew",
+  description: "Radio, Reimagined",
+  openGraph: {
+    title: "Whew",
+    description: "Radio, Reimagined",
+    url: "https://whew-music.vercel.app", // swap with your real domain
+    siteName: "Whew",
+    images: [
+      {
+        url: "./opengraph.png", // place hero.png inside /public
+        width: 1200,
+        height: 630,
+        alt: "Whew Hero",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+};
+
+
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL);
 
 export default function RootLayout({ children }) {
